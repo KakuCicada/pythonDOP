7179e74ccb7b8604a7acb11089a8a7c4fe48529c
LAPTOP-MNAGI07Q\ema <meng.guo@emagroup.cn>
2018-07-06 11:19:45 +0800

    第一个项目，百度翻译 后续作为GUI形式

---
+++ fanyi_baidu/settings.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+# 百度API相关配置信息
+baiDuAppId = '20180703000182418'
+baiDuSecretKey = 'TTOEu9fCY3YHZXkS9Tg2'
+baiDuApiUrl = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
