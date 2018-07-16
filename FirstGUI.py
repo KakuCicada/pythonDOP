9e2d60a31812f32d19a302842694846399ca9e38
LAPTOP-MNAGI07Q\ema <meng.guo@emagroup.cn>
2018-07-16 10:44:52 +0800

    添加状态栏菜单和退出动作，以及设置退出动作的快捷键

--- FirstGUI.py
+++ FirstGUI.py
@@ -1,28 +1,39 @@
-#-*- conding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QDesktopWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp
+from PyQt5.QtGui import QIcon
 
-class Example(QWidget):
+class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.resize(350,250)
-        self.center()
-        self.setWindowTitle('Center')
+        exitAction = QAction(QIcon('logout.png'), '&退出', self)
+        # 设置对应快捷键
+        exitAction.setShortcut('Ctrl+Q')
+        # 设置状态栏提示信息
+        exitAction.setStatusTip('退出应用程序')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        # 添加一个菜单，显示为文件
+        fileMenu = menubar.addMenu('&文件')
+        # 将自定义的抽象动作，添加至文件菜单中
+        fileMenu.addAction(exitAction)
+        EditeMenu = menubar.addMenu('&编辑')
+        EditeMenu.addAction(exitAction)
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('菜单栏')
         self.show()
 
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
