1017b84075a020bd938de9445392a5aca898353a
LAPTOP-MNAGI07Q\ema <meng.guo@emagroup.cn>
2018-08-03 16:31:27 +0800

    test提交

--- TestObj/SocketFTP/FTPClient.py
+++ TestObj/SocketFTP/FTPClient.py
@@ -15,29 +15,54 @@ ftpClient.connect(ipPort)
 def Userinfo():
     user = input('请输入用户名：').strip()
     passwd = input('输入该用户密码：').strip()
-    info = user + ' ' + passwd
+    info = 'login ' + user + ' ' + passwd
     return info
 
-def StreamPro(Action):
+def StreamPush(Action):
     Pack = struct.pack('i', len(Action))
     ftpClient.send(Pack)
     ftpClient.send(Action.encode('utf-8'))
+
+def StreamPull():
     packlen = ftpClient.recv(4)
+    # print(packlen)
     Meslen = struct.unpack('i', packlen)[0]
+    # print(Meslen)
     ServMes = ftpClient.recv(Meslen)
-    ####
-    print(type(ServMes))
-    ####
     PackMes = ServMes.decode('utf-8')
-    print(PackMes.split(' ')[1:])
-    return PackMes.split(' ')[0]
+    return PackMes.split(' ')
 
 
-Help = '帮助信息：\nlogin\t必须登录账号密码\nls\t显示当前目录下的文件\nupload\t上传文件\ndown\t下载文件\ncd\t更换目录'
+Help = '帮助信息：\nlogin\t必须登录账号密码\nls\t显示当前目录下的文件\nupload\t上传文件\ndown\t下载文件\ncd\t更换目录\nquit|exit\t退出当前用户'
 print(Help)
 
-while True:
-    usercmd = input('请输入要执行的命令：').strip()
-    if usercmd == 'login':
-        UserMes = Userinfo()
-        StreamPro(UserMes)
+# 首先验证用户合法性
+UserMes = Userinfo()
+StreamPush(UserMes)
+L = StreamPull()
+CmdList = [['ls','cd'],['quit','exit']]
+if L[0]:
+    while True:
+        usercmd = input('请输入要执行的命令：').strip()
+        if not usercmd:continue
+        command = usercmd.split()[0]
+        if command in CmdList[0]:
+            StreamPush(usercmd)
+            L = StreamPull()
+            print(' '.split(L))
+        elif command in CmdList[1]:
+            pass
+        elif command == 'upload':
+            StreamPush(usercmd)
+            filename = usercmd.split()[1]
+            # 读取文件内容，并上传值服务器
+            with open(filename) as f:
+                FileStr = f.read()
+            StreamPush(FileStr)
+            L = StreamPull()
+            print(' '.split(L))
+        elif command == 'down':
+            pass
+        else:
+            print('输入命令错误，请重新输入')
+            continue
--- TestObj/SocketFTP/FtpServer.py
+++ TestObj/SocketFTP/FtpServer.py
@@ -9,27 +9,45 @@ import settings
 ftpServer = socket(AF_INET,SOCK_STREAM)
 
 ipPort = (settings.ServerMes['ip'],settings.ServerMes['port'])
+SetUser = settings.UserMes
 
-class clientCMD(object):
-    '''存储来自于client的命令方法
-    '''
-    def __init__(self, arg):
-        self.arg = arg
+def Changedir(Mes):
+    Dir = Mes[1]
+    os.chdir(Dir)
+    mes = '目录切换至 %s 成功' %Dir
+    print(mes)
+    return mes
 
-    def Changedir(self):
-        Dir = self.arg
-        os.chdir(Dir)
-        Mes = '目录切换至 %s 成功' %Dir
-        return Mes
+def uploadfile(request,Mes):
+    Filename = Mes[1]
+    Fileread = request.recv()
+    with open(str(Filename),'rw+') as f:
+        f.write(Fileread)
+    return '上传文件成功'
 
-    def uploadfile(self):
-        pass
+def downloadfile(request,Mes):
+    pass
 
-    def downloadfile(self):
-        pass
+def showfile(Mes):
+    if len(Mes) == 1:
+        RetMes = os.listdir()
+    else:
+        RetMes = os.listdir(Mes[1])
+    return ' '.join(RetMes)
 
-    def showfile(self):
-        pass
+def login(Mes):
+    # 读取配置文件里的信息，获取输入过来的账号密码作对比
+    if Mes[1] in SetUser:
+        if Mes[2] == SetUser[Mes[1]]['passwd']:
+            mes = '0 登录成功'
+            PWD = os.getcwd()
+            UserHomeDir = PWD + SetUser[Mes[1]]['homedir']
+            os.chdir(UserHomeDir)
+        else:
+            mes = '1 登录失败'
+    else:
+        mes = '1 用户不存在'
+    return mes
 
 def Getcmd(request):
     meslen = request.recv(4)
@@ -39,10 +57,11 @@ def Getcmd(request):
     return SendInfo
 
 def SendMes(request,Mes):
-    Pack = struct.pack('i', len(Mes))
-    print(Pack)
+    Pack = struct.pack('i', len(str(Mes).encode('utf-8')))
+    # print(Pack)
     request.send(Pack)
-    request.send(Mes.encode('utf-8'))
+    request.send(str(Mes).encode('utf-8'))
+    print(str(Mes).encode('utf-8'))
 
 
 ftpServer.bind(ipPort)
@@ -51,22 +70,34 @@ print('服务器开始运行于%s的%s端口' %(settings.ServerMes['ip'],setting
 while True:
     conn,addr = ftpServer.accept()
     print('新的客户端连接：',addr)
+    while True:
     # try:
         # 当有新的链接进入时，需要进行登录操作
-    Info = Getcmd(conn)
-    SetUser = settings.UserMes
-    if Info[0] in SetUser:
-        if Info[1] == SetUser[Info[0]]['passwd']:
-            mes = '0 登录成功'
-        else:
-            mes = '1 登录失败'
-    else:
-        mes = '1 用户不存在'
-    print('*'*40)
-    SendMes(conn,mes)
+        Info = Getcmd(conn)
+        print(Info)
+        print('*' * 40)
+        if Info[0] == 'login':
+            Mes = login(Info)
+            SendMes(conn, Mes)
+        elif Info[0] == 'cd':
+            Mes = Changedir(Info)
+            SendMes(conn, Mes)
+        elif Info[0] == 'upload':
+            Mes = '开始传输文件'
+            SendMes(conn, Mes)
+            uploadfile(conn,Info[1])
 
-    while True:
-        pass
+        elif Info[0] == 'down':
+            Mes = downloadfile(Info)
+        elif Info[0] == 'ls':
+            Mes = showfile(Info)
+            SendMes(conn, Mes)
+        elif Info[0] == 'quit' or Info[0] =='exit':
+            conn.close()
+            break
+
+
+
     # except Exception as e:
     #     print(e)
     #     break
