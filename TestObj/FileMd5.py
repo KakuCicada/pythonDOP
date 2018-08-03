e7ad2a7b40fe7c8c6291cc48992d5b20d9099433
LAPTOP-MNAGI07Q\ema <meng.guo@emagroup.cn>
2018-08-03 18:29:08 +0800

    MD5 文件计算

--- TestObj/FileMd5.py
+++ TestObj/FileMd5.py
@@ -1,5 +1,25 @@
 # -*- coding:utf-8 -*-
 
 import os, sys
-from hashlib import md5
+import hashlib
 
+File = 'GetIP.py'
+
+md5 = hashlib.md5
+
+def HashStr(file):
+    while True:
+        Str = file.read(10240)
+        if not Str:
+            break
+        md5.update(Str.encode('utf-8'))
+    return md5.hexdigest()
+
+
+with open(File) as f:
+    MD5 = HashStr(f)
+    print(MD5)
+
+# with open(File) as f:
+#     data = f.read()
+#     print(md5(data.encode('utf-8')).hexdigest())
