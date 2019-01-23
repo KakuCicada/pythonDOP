20fc6cb652889fb127e3c9265603c4b4c8c0763d
LAPTOP-MNAGI07Q\ema <meng.guo@emagroup.cn>
2019-01-23 19:51:07 +0800

    修改样式，出现title

--- WebTool/server/views.py
+++ WebTool/server/views.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, render_to_response
 
+hometitle='服务器管理系统'
 # Create your views here.
 def homepage(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', {'title': hometitle,'username': 'Kaku'})
 
 def basepage(request):
     return render_to_response('base.html')
